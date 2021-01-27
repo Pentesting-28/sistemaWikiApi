e2b9db1d690a664b93567febe2ca4349aa98714a
Mario <mpenav28@gmail.com>
2021-01-26 21:44:39 -0400

    Creacion controlador UserController

---
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try {
+            $data User::where('id', '<>', Auth::id())->get();
+            return response()->json([
+                'message' => 'Lista de usuarios',
+                'data'    => $data
+            ],200);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'users.index.failed',
+                'message'=> $e->getMessage(),
+            ],500);
+        }}
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
