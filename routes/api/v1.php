a4d7460942905aad366d4acda8987f4f8d7d8a5a
Mario <mpenav28@gmail.com>
2021-01-26 21:45:24 -0400

    Creacion ruta api para UserController

---
+++ routes/api/v1.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::name("api.v1")->group(function () {
+
+	// Authentication
+	// Route::prefix("auth")->namespace("Auth")->group(function () {
+	// 	Route::post('login', 'AuthController@login');
+	// 	Route::post('logout', 'AuthController@logout')->middleware(["auth:api"]);
+
+	// });
+    // User
+    Route::prefix("users")->group(function () {
+        Route::get("/", "UserController@index");
+        // Route::post("/store", "UserController@store");
+        // Route::get("/{user}", "UserController@show");
+        // Route::put("/{id}", "UserController@update");
+        // Route::delete("/{user}", "UserController@delete");
+    });
+
+}); 
