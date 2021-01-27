366cbcf8cef2bbb03fb45baf968847f6d82180f6
Pentesting-28 <mpenav28@gmail.com>
2021-01-27 14:14:58 -0400

    Creud Usuarios

--- app/Http/Controllers/Api/v1/UserController.php
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers\Api\v1;
 
+use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Exception;
 
 class UserController extends Controller
 {
@@ -16,7 +20,7 @@ class UserController extends Controller
     public function index()
     {
         try {
-            $data User::where('id', '<>', Auth::id())->get();
+            $data = User::where('id', '<>', Auth::id())->get();
             return response()->json([
                 'message' => 'Lista de usuarios',
                 'data'    => $data
@@ -26,7 +30,7 @@ class UserController extends Controller
                 'error' => 'users.index.failed',
                 'message'=> $e->getMessage(),
             ],500);
-        }}
+        }
     }
 
     /**
@@ -37,7 +41,30 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validator = Validator::make($request->all(), [
+                'name'     => 'required|string|max:255',
+                'email'    => 'required|string|email|unique:users',
+                'password' => 'required|string|min:6|confirmed',
+                'password_confirmation' => 'required|same:password'
+            ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    'error'=>$validator->errors()
+                ], 422);
+            }
+            $request['password'] = Hash::make($request->password);
+            $data = User::create($request->all());
+            return response()->json([
+                'message' => 'Usuario creado con éxito',
+                'data'    => $data
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'error'    => 'user.store.failed',
+                'message'  => $e->getMessage()
+            ], 505);
+        }
     }
 
     /**
@@ -48,7 +75,21 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        try {
+            if($id == Auth::id()){
+                throw new Exception('Usuario no encontrado', 505);
+            }
+            $data = User::findOrFail($id);
+            return response()->json([
+                "message" =>'Detalles de usuario',
+                "data" => $data,
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'error'  => 'users.show.failed',
+                'message'=> $e->getMessage(),
+            ], 505);
+        }
     }
 
     /**
@@ -60,7 +101,31 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        try {
+            if($id == Auth::id()){
+                throw new Exception('Usuario no encontrado', 505);
+            }
+            $validator = Validator::make($request->all(), [
+                'name'  => 'required|string|max:255',
+                'email' => "required|string|email|max:255|unique:users,email,$id"
+            ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    'error'=>$validator->errors()
+                ], 422);
+            }
+            $data = User::findOrFail($id);
+            $data->update($request->all());
+            return response()->json([
+                'message' => 'Usuario actualizado con éxito',
+                'data'    => $data
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'error'    => 'user.update.failed',
+                'message'  => $e->getMessage()
+            ], 505);
+        }
     }
 
     /**
@@ -71,6 +136,21 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        try {
+            if($id == Auth::id()){
+                throw new Exception('Usuario no encontrado', 505);
+            }
+            $data = User::findOrFail($id);
+            $data->delete();
+            return response()->json([
+                'message' => 'Usuario eliminado con éxito',
+                'data'    => $data
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'error'   => 'user.destroy.failed',
+                'message' => $e->getMessage()
+            ], 505);
+        }
     }
 }
